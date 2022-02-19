93ea62d091fca89fe26733a8451f9dc4a0409ebe
Özgür Uysal <ozguruysal@gmail.com>
2022-02-19 14:45:53 +0300

    Tailwindcss color palette and shadow comfigs

--- components/creator-card.tsx
+++ components/creator-card.tsx
@@ -10,14 +10,14 @@ export const CreatorCard: FunctionComponent<Props> = ({ creator }) => {
   return (
     <a
       href={`/${creator.username}`}
-      className="p-6 flex flex-col items-center border hover:border-neutral-300 hover:shadow-sm rounded"
+      className="p-6 flex flex-col items-center border hover:border-gray-300 hover:shadow-sm rounded"
     >
       {creator.avatar && (
         <Avatar src={creator.avatar?.url} alt={creator.fullName} size={64} />
       )}
 
       <h3 className="mt-4 text-xl font-bold text-black">{creator.fullName}</h3>
-      <div className="mt-0.5 text-neutral-600">@{creator.username}</div>
+      <div className="mt-0.5 text-gray-600">@{creator.username}</div>
     </a>
   );
 };
