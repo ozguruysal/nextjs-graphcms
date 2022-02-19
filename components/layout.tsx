54d2f30c9e918030e482ab39909c1b8c737bd411
Özgür Uysal <ozguruysal@gmail.com>
2022-02-19 22:25:06 +0300

    Update layout component

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,8 +2,8 @@ import { FunctionComponent } from "react";
 
 export const MainLayout: FunctionComponent = ({ children }) => {
   return (
-    <div className="text-base antialiased font-body bg-white text-gray-700">
-      <main className="py-4 md:py-8 px-4 md:px-8 mx-auto max-w-screen-xl">
+    <div >
+      <main className="py-12 px-5 md:px-8 mx-auto max-w-screen-xl">
         {children}
       </main>
     </div>
