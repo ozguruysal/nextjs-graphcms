a15edfdafb8264d34a5f32f90c7646d892217831
Özgür Uysal <ozguruysal@users.noreply.github.com>
2022-02-20 19:04:41 +0300

    Add splitbee (#3)
    * Implement SEO component
    
    * Experimenting with splitbee
    
    * Use nextjs proxy for splitbee
    
    * Update readme etc

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
   images: {
     domains: ["media.graphcms.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import splitbee from "@splitbee/web";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: "/bee.js",
+      apiUrl: "/_hive",
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
