3457d952da5a4d75967a7f44e1a1a95a675f54ee
Özgür Uysal <ozguruysal@gmail.com>
2022-02-20 17:31:59 +0300

    Implement SEO component

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,5 @@ export { Avatar } from "./avatar";
 export { CreatorCard } from "./creator-card";
 export { LinkBlock } from "./link-block";
 export { MainLayout } from "./layout";
+export { Meta } from "./meta";
 export { VideoBlock } from "./video-block";
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -2,7 +2,7 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import Link from "next/link";
 import { ParsedUrlQuery } from "querystring";
 import { FunctionComponent } from "react";
-import { Avatar, LinkBlock, MainLayout, VideoBlock } from "../components";
+import { Avatar, LinkBlock, MainLayout, Meta, VideoBlock } from "../components";
 import { client } from "../lib/graphcms-client";
 import { creatorByUsernameQuery, allCreatorSlugsQuery } from "../lib/queries";
 import type { Creator, LinkBlockType, VideoBlockType } from "../types";
@@ -54,49 +54,61 @@ const Creator: FunctionComponent<Props> = ({ creator }) => {
   }
 
   return (
-    <MainLayout>
-      <div className="max-w-[25rem] mx-auto">
-        <div className="flex flex-col items-center text-center">
-          <Avatar
-            src={creator.avatar?.url as string}
-            alt={creator.fullName}
-            size={100}
-          />
+    <>
+      <Meta
+        title={creator.fullName}
+        description={creator.bio}
+        ogimage={creator.avatar?.url}
+      />
 
-          <h3 className="mt-4 text-xl font-bold text-black leading-6">
-            {creator.fullName}
-          </h3>
+      <MainLayout>
+        <div className="max-w-[25rem] mx-auto">
+          <div className="flex flex-col items-center text-center">
+            <Avatar
+              src={creator.avatar?.url as string}
+              alt={creator.fullName}
+              size={100}
+            />
 
-          {creator.bio && (
-            <div className="mt-0.5 text-gray-600 leading-5">{creator.bio}</div>
-          )}
+            <h3 className="mt-4 text-xl font-bold text-black leading-6">
+              {creator.fullName}
+            </h3>
+
+            {creator.bio && (
+              <div className="mt-0.5 text-gray-600 leading-5">
+                {creator.bio}
+              </div>
+            )}
+          </div>
+
+          <div className="mt-7 space-y-4">
+            {creator.blocks?.length &&
+              creator.blocks.map((block) => {
+                if (block.__typename === "LinkBlock") {
+                  return (
+                    <LinkBlock
+                      key={block.id}
+                      label={(block as LinkBlockType).label}
+                      url={block.url}
+                    />
+                  );
+                } else if (block.__typename === "VideoBlock") {
+                  return (
+                    <VideoBlock
+                      key={(block as VideoBlockType).id}
+                      url={block.url}
+                    />
+                  );
+                }
+              })}
+          </div>
+
+          <Link href="/">
+            <a className="mt-12 inline-block text-primary-500">&#8592; Home</a>
+          </Link>
         </div>
-
-        <div className="mt-7 space-y-4">
-          {creator.blocks?.length &&
-            creator.blocks.map((block) => {
-              if (block.__typename === "LinkBlock") {
-                return (
-                  <LinkBlock
-                    key={block.id}
-                    label={(block as LinkBlockType).label}
-                    url={block.url}
-                  />
-                );
-              } else if (block.__typename === "VideoBlock") {
-                return (
-                  <VideoBlock
-                    key={(block as VideoBlockType).id}
-                    url={block.url}
-                  />
-                );
-              }
-            })}
-        </div>
-
-        <Link href="/"><a className="mt-12 inline-block text-primary-500">&#8592; Home</a></Link>
-      </div>
-    </MainLayout>
+      </MainLayout>
+    </>
   );
 };
 
